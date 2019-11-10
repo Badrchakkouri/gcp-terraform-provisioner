f2b3493bd40fb8f1dca64d3874ce500383036a23
Badr Chakkouri <badr.chakkouri@gmail.com>
2019-11-10 22:01:32 +0000

    update

--- user_data.sh
+++ user_data.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-yum -y install epel-release
-yum -y install nginx
+sudo yum -y install epel-release
+sudo yum -y install nginx
 cd /usr/share/nginx/html/
-mv index.html index.html.old
+sudo mv index.html index.html.old
 echo "
 <h1 style=\"text-align: center;\"><strong>Badr says hello from GCP!&nbsp;</strong><span></span></h1>
 <p><span><img src=\"https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif\" alt=\"\" width=\"480\" height=\"360\" style=\"display: block; margin-left: auto; margin-right: auto;\" /></span></p>
-" | tee -a index.html
-chmod 644 index.html
-systemctl restart nginx
+" | sudo tee -a index.html
+sudo chmod 644 index.html
+sudo systemctl restart nginx
